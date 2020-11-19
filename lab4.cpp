ac21232e2ccfa6e6fc487c73927d4fc17ed80bf5
piotrmikula <p.mikula14@wp.pl>
2020-11-19 12:10:49 +0000

    Zadanie2

--- lab4.cpp
+++ lab4.cpp
@@ -3,11 +3,18 @@
 template < typename T >
 class Para
 {
+public:
     T obiekt1;
     T obiekt2;
+    T suma() { return obiekt1 + obiekt2; }
 };
 
 int main()
 {
+    Para< double > parka;
+    parka.obiekt1 = 3.14;
+    parka.obiekt2 = 2.11;
+    std::cout << "Suma rowna jest " << parka.suma() << std::endl;
+
     puts("Ostatnia linijka w kodzie!");
 }
