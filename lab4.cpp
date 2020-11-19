f596d97155b178c81b5562be331eb1d141be8174
piotrmikula <p.mikula14@wp.pl>
2020-11-19 12:10:31 +0000

    Zadanie1

--- lab4.cpp
+++ lab4.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+template < typename T >
+class Para
+{
+    T obiekt1;
+    T obiekt2;
+};
+
 int main()
 {
-    puts("Hello, World!");
+    puts("Ostatnia linijka w kodzie!");
 }
