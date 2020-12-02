c3ba39de2cdaf8e9a965490cab0e9d6584585ad1
piotrmikula <p.mikula14@wp.pl>
2020-12-02 10:40:41 +0000

    Zadanie5

--- lab4.cpp
+++ lab4.cpp
@@ -17,14 +17,31 @@ class TablicaPar
 public:
     TablicaPar() {}
     Para< T >  tablica[N];
-    TablicaPar operator[](int n) { return tablica[n]; }
+    Para< T >& operator[](int n) { return tablica[n]; }
+
+    void print()
+    {
+        for (unsigned int i = 0; i < N; i++)
+            std::cout << tablica[i].obiekt1 << "\t" << tablica[i].obiekt2 << std::endl;
+    }
 };
 
 int main()
 {
-    Para< double >          parka(1, 2);
-    TablicaPar< double, 4 > tab;
+    Para< double >          p1{2, 3};
+    Para< double >          p2{3, 5};
+    Para< double >          p3{4, 10};
+    TablicaPar< double, 3 > tab;
     std::cout << "Rozmiar tablicy to " << sizeof(tab.tablica) << std::endl;
+
+    tab[0] = p1;
+    tab[1] = p2;
+    tab[2] = p3;
+
+    tab.print();
+
+    double suma = p1.suma() + p2.suma() + p3.suma();
+    std::cout << "Suma wszystkich par wynosi " << suma << std::endl;
 
     puts("\nOstatnia linijka w kodzie!");
 }
